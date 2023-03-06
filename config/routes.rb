05eab531d014adfee91f1f37b71bc4f92208735b
Louis Charbonnier <charbonnierlouis@gmail.com>
2023-03-06 16:57:38 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :leagues do
+    resources :challenges, only: [:show, :index]
+    resources :user_leagues, only: [:index, :show]
+  end
+  resources :users, only: [:show, :edit, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
