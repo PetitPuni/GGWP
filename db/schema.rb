3b92401fdd18fd530e7ec32ce443df573664a616
jonathan rouas <rouasjonathan@gmail.com>
2023-03-07 17:39:50 +0100

    let's go

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_144728) do
-=======
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_10_112157) do
->>>>>>> 24ffd050fcf117ad7f224b487b7a9dfffb6ac5e8
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
