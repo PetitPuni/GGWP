7a75c99eaeb2de4ec83160c854cfe6af35f5d390
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-07 12:20:22 +0100

    Did the show & index of the league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -10,10 +10,17 @@ class LeaguesController < ApplicationController
 
     def new
         @league = League.new
+        @games = Game.all.map { |game| game.name }
     end
     
     def create
-        League.create(league_params)
+        @league = League.new(league_params)
+        @league.user = current_user
+        if @league.save!
+            redirect_to league_path(@league)
+        else
+            render :new, status: :unrpocessable_entity
+        end
     end
     
     
