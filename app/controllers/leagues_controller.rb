70ae40eac90c22ae8d4186eeb59c21cefdddc790
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-07 13:03:33 +0100

    You can create a League

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -10,13 +10,14 @@ class LeaguesController < ApplicationController
 
     def new
         @league = League.new
-        @games = Game.all.map { |game| game.name }
+        # @games = Game.all.map { |game| game.name }
+        @games = Game.all
     end
     
     def create
         @league = League.new(league_params)
-        @league.user = current_user
         if @league.save!
+            user_league = UserLeague.new(user: current_user, league: @league)
             redirect_to league_path(@league)
         else
             render :new, status: :unrpocessable_entity
@@ -38,7 +39,7 @@ class LeaguesController < ApplicationController
     private
 
     def league_params
-        params.require(:league).permit(:name, :description, :start_on, :end_on)
+        params.require(:league).permit(:name, :description, :start_on, :end_on, :game_id)
     end
 
     def set_league
