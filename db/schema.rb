2a078581058e8c1905dfb1444ff093fbace66402
Louis Charbonnier <charbonnierlouis@gmail.com>
2023-03-08 11:08:38 +0100

    add column token on League, add ranbomToken generator gem and add a token to the first league

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_131133) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_100257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_131133) do
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
     t.index ["game_id"], name: "index_leagues_on_game_id"
   end
 
