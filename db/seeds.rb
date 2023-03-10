2b1e6136a9344f9415f8f39b4f28376035049eb6
Louis Charbonnier <charbonnierlouis@gmail.com>
2023-03-10 15:56:14 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,90 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts 'destroying all by replanting'
-
-ap User.all
-
-# puts 'creating user, league, challenge, user_league, user_league_challenge'
-
-# user1 = User.create!(
-#   steam_id: '76561197981067382'
-# )
-
-# ap User.all
-
-# ap 'user created'
-
-# game = Game.create!(
-#   name: "Counter-Strike: Global Offensive",
-#   genre: "FPS",
-#   description: "Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, and weapons and delivers updated versions of the classic CS content (de_dust, etc.). In addition, CS: GO will introduce new gameplay modes, matchmaking, leader boards, and more.",
-#   app_id: 730
-# )
-
-# ap 'game created'
-
-# league = League.create!(
-#   name: "League 1",
-#   description: "the first league of the batch #1102",
-#   start_on: Date.today,
-#   end_on: Date.today + 1.month,
-#   game: game,
-#   token: RandomToken.gen(6)
-# )
-
-# ap 'league created'
-
-# challenge = Challenge.create!(
-#   name: "Kill 10 enemies",
-#   description: "Kill 10 enemies with a pistol",
-#   points: 10,
-#   key: 1,
-#   game: game
-# )
-
-# challenge1 = Challenge.create!(
-#   name: "defuse the bomb",
-#   description: "defuse the bomb",
-#   points: 10,
-#   key: 2,
-#   game: game
-# )
-
-# challenge2 = Challenge.create!(
-#   name: "plant the bomb",
-#   description: "plant the bomb",
-#   points: 10,
-#   key: 3,
-#   game: game
-# )
-
-# ap 'challenge created'
-
-# user_league = UserLeague.create!(
-#   user: user1,
-#   league: league
-# )
-
-# ap 'user_league created'
-
-# UserLeagueChallenge.create!(
-#   challenge: challenge,
-#   user_league: user_league
-# )
-
-# UserLeagueChallenge.create!(
-#   challenge: challenge1,
-#   user_league: user_league
-# )
-
-# UserLeagueChallenge.create!(
-#   challenge: challenge2,
-#   user_league: user_league
-# )
+puts 'destroying all'
 
 
-# ap 'user_league_challenge created'
+
+game = Game.create!(
+  name: "Counter-Strike: Global Offensive",
+  genre: "FPS",
+  description: "Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, and weapons and delivers updated versions of the classic CS content (de_dust, etc.). In addition, CS: GO will introduce new gameplay modes, matchmaking, leader boards, and more.",
+  app_id: 730
+)
+
+ap 'user_league_challenge created'
 
 #  VARIABLE USED FOR THE CHALLENGE GENERATOR
 
