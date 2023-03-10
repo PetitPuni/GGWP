e61fbaabd8ddd0154f76d6c5a63bb4470395264c
Valentin Beau <valentinbeau.fr@gmail.com>
2023-03-10 14:53:40 +0100

    added background when signing in + changed the front for my league

--- db/seeds.rb
+++ db/seeds.rb
@@ -122,7 +122,7 @@ actions.each do |action|
       name: "#{ApplicationController.helpers.action_capitalize(action)} #{ennemies} #{ApplicationController.helpers.translate_weapon(gun)}",
       description: "Pour cette mission si vous l'acceptez you need to #{ApplicationController.helpers.action_capitalize(action)} #{ennemies} ennemies with this #{ApplicationController.helpers.translate_weapon(gun)} !",
       points: (1..100).to_a.sample,
-      game: game
+      game: game,
       ennemies: ennemies,
       gun: gun,
       action: action
