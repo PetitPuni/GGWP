1f7294c36d8783a66541e862b4ffcb47670fae0f
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-13 14:38:55 +0100

    Finished the timer. Finally ! Did a few css modification as well as text modification

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -9,11 +9,11 @@ export default class extends Controller {
     start: String
   }
   
-  static targets = ['days', 'hours', 'minutes', "seconds"]
+  static targets = ['days', 'hours', 'minutes', "seconds", "affichage", "endmessage"]
   
   connect() {
     this.start = new Date(this.startValue)
-    console.log(this.startValue, this.start)
+    // console.log(this.startValue, this.start)
     setInterval(this.refresh, 1000)
   }
   
@@ -21,14 +21,22 @@ export default class extends Controller {
     this.refresh = this.refresh.bind(this)
   }
   
+  show(distance){
+    this.daysTarget.innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+    this.hoursTarget.innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutesTarget.innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    this.secondsTarget.innerText = Math.floor((distance % (1000 * 60)) / 1000);
+  }
+
+  showEndMessage() {
+    // this.endmessageTarget.innerText = "The league is over ! Check who won 🏆!"
+  }
+
 	refresh() {
     const now = (new Date());
-    console.log(now)
-    let distance = Math.abs(this.start.getTime()  - now.getTime());
-    
-    this.daysTarget.innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
-  	this.hoursTarget.innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  	this.minutesTarget.innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  	this.secondsTarget.innerText = Math.floor((distance % (1000 * 60)) / 1000);
+    // console.log(now)
+    let distance = this.start.getTime() - now.getTime();
+    console.log(distance)
+    distance >= 0 ? this.show(distance) : this.showEndMessage()
   }
 }
